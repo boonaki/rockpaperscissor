4160272e9de237c0c075d3d3f44c5db250ff9cd9
Ryan Mahara <ryanmahara@gmail.com>
2022-05-26 20:12:17 -0500

    add input validation

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@ document.querySelector('#shoot').addEventListener('click', makeReq)
 async function makeReq(){
 
   const playerChoice = document.querySelector("#playerSelection").value;
+  
+    const gameSet = ['rock', 'paper', 'scissors'];
+  if(!gameSet.includes(playerChoice.toLowerCase())) return;
   const res = await fetch(`/api?pick=${playerChoice}`)
   const data = await res.json()
 
